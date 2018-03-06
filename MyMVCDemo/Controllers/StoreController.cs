183bb9cd2255f0ebe330e78e5ac369dd56a4fb3e
Admin <Admin@ADMIN-PC>
2018-03-07 10:58:00 +1300

    Add project files.

---
+++ MyMVCDemo/Controllers/StoreController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMVCDemo.Models;
+
+namespace MyMVCDemo.Controllers
+{
+    public class StoreController : Controller
+    {
+        private List<TransactionHead> header;
+        public StoreController()
+        {
+            using (var db=new LINQExercisesEntities())
+            {
+                header = db.TransactionHead.Include("TransactionLine.Product")
+                                         .Include("Customer")
+                                         .Include("Staff")
+                                         .Include("Store")
+                                         .ToList();
+            }
+        }
+
+        public ActionResult Index()
+        {
+            
+            return View(header);
+        }
+        public ActionResult StoreList()
+        {
+            
+            var stores=header.Select(x=>x.Store).Distinct();
+
+            return View(stores);
+
+        }
+        public ActionResult TransactionDetail(int id=1)
+        {
+          
+            return View(header);
+        }
+        public ActionResult CustomerList()
+        {
+            var customers = header.Select(x=>x.Customer);
+            return View(customers);
+        }
+        public ActionResult Action()
+        {
+            return View();
+        }
+    }
+}
